bbd5820d1bd3d03707e744399897fcda89feede4
Guido Günther <agx@sigxcpu.org>
2022-02-25 08:37:23 +0100

    top-panel: Fix namespace
    Signed-off-by: Guido Günther <guido.gunther@puri.sm>

--- src/top-panel.c
+++ src/top-panel.c
@@ -613,7 +613,7 @@ phosh_top_panel_new (struct zwlr_layer_shell_v1          *layer_shell,
                        "layer", ZWLR_LAYER_SHELL_V1_LAYER_TOP,
                        "kbd-interactivity", FALSE,
                        "exclusive-zone", PHOSH_TOP_PANEL_HEIGHT,
-                       "namespace", "phosh",
+                       "namespace", "phosh top-panel",
                        NULL);
 }
 
